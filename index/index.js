c14a39f9bf78f65c70c807993c039b35ed762488
wisdomwindy <1659140773@qq.com>
2022-10-26 21:21:39 +0800

    更新文件

--- index/index.js
+++ index/index.js
@@ -1,31 +1,46 @@
 const app = getApp()
 const upng = require('upng-js');
-const { default: jsQR } = require('jsqr');
+const {
+	default: jsQR
+} = require('jsqr');
 Page({
-  data: {
+	data: {
 
-  },
-  onLoad() {
 	},
-	handleLongpress(e){
-		const {
-			canvasid:canvasId
-		} = e.currentTarget.dataset;
-		wx.createSelectorQuery().select('#canvas').fields({node:true}).exec(res => {
+	onLoad() {
+		wx.createSelectorQuery().select('#canvas').fields({
+			node: true
+		}).exec(res => {
 			const canvas = res[0].node;
 			const ctx = canvas.getContext('2d');
 			const img = canvas.createImage();
 			img.src = '../imgs/hello+world.png';
-			img.onload = function (){
-				ctx.drawImage(img,0,0,canvas.width,canvas.height);
-				console.log(canvasId, img, img.width,img.width, canvas.width,canvas.height);
+			img.onload = () => {
+				ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 				let url = canvas.toDataURL();
-				let buff = wx.base64ToArrayBuffer(url.split(',')[1]);
-				let r = upng.decode(buff);
-				let rgba = upng.toRGBA8(r)[0];
-				let code = jsQR(new Uint8ClampedArray(rgba),canvas.width,canvas.height);
-				console.log(code.data)
+				this.setData({
+					imgUrl: url
+				});
 			}
-		});	
+		});
+	},
+	handleimgLongpress(e) {
+		let {
+			url: url
+		} = e.currentTarget.dataset;
+		wx.showActionSheet({
+			itemList: ['识别二维码'],
+			success: (result) => {
+				if(result.tapIndex === 0){
+					let buff = wx.base64ToArrayBuffer(url.split(',')[1]);
+					let r = upng.decode(buff);
+					let rgba = upng.toRGBA8(r)[0];
+					let code = jsQR(new Uint8ClampedArray(rgba), r.width,r.height);
+					console.log(code.data)
+				}
+			},
+			fail: (res) => {},
+			complete: (res) => {},
+		})
 	}
 })
